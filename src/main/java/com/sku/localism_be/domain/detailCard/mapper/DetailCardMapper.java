845a1f76e7d9c8c65f83e84cba41abaecbb4dabe
SunSeoYeon <ssy01157@gmail.com>
2025-08-11 00:28:21 +0900

    mapper 수정

--- src/main/java/com/sku/localism_be/domain/detailCard/mapper/DetailCardMapper.java
+++ src/main/java/com/sku/localism_be/domain/detailCard/mapper/DetailCardMapper.java
@@ -2,12 +2,29 @@ package com.sku.localism_be.domain.detailCard.mapper;
 
 
 import com.sku.localism_be.domain.detailCard.dto.response.InputReportResponse;
+import com.sku.localism_be.domain.detailCard.dto.response.RtsResponse;
 import com.sku.localism_be.domain.detailCard.dto.response.SmallReportResponse;
 import com.sku.localism_be.domain.detailCard.entity.DetailCard;
+import com.sku.localism_be.domain.detailCard.entity.Rts;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DetailCardMapper {
+
+  public RtsResponse toRtsResponse(Rts rts) {
+    return RtsResponse.builder()
+        .respirationRate(rts.getRespirationRate())
+        .respirationScore(rts.getRespirationScore())
+        .bloodPressureMin(rts.getBloodPressureMin())
+        .bloodPressureMax(rts.getBloodPressureMax())
+        .bloodPressureScore(rts.getBloodPressureScore())
+        .consciousness(rts.getConsciousness())
+        .consciousnessScore(rts.getConsciousnessScore())
+        .RTSScore(rts.getTotalScore())
+        .currentStatus(rts.getCurrentStatus())
+        .summary(rts.getSummary())
+        .build();
+  }
 
   public InputReportResponse toInputReportResponse(DetailCard detailCard) {
     return InputReportResponse.builder()
@@ -37,8 +54,6 @@ public class DetailCardMapper {
         .aiRecommendedAction(detailCard.sliceAi())
         .build();
   }
-
-
   public SmallReportResponse toSmallReportResponse(DetailCard detailCard) {
     return SmallReportResponse.builder()
         .id(detailCard.getId())
@@ -54,5 +69,6 @@ public class DetailCardMapper {
         .majorSymptoms(detailCard.sliceMS())
         .build();
   }
+
 
 }
